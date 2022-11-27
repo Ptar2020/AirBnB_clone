45efaa417868e8df3015e17d2f1d18221fdc243c
Ptar2020 <peterolwande@gmail.com>
2022-11-27 18:47:56 +0300

    Finish all Tasksk

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""The console class"""
 import cmd
 import time
 import sys
@@ -10,7 +11,6 @@ from models.state import State
 from models.place import Place
 from models.amenity import Amenity
 import models
-"""The console class"""
 
 
 class HBNBCommand(cmd.Cmd):
@@ -58,14 +58,78 @@ class HBNBCommand(cmd.Cmd):
         else:
             print('** instance id missing **')
 
+    def do_all(self, line):
+        """ Method to print all instances """
+        if len(line) == 0:
+            print([str(a) for a in models.data.all().values()])
+        elif line not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            print([str(a) for b, a in models.data.all().items() if line in b])
+
     def do_update(self, line):
-        pass
+        """ Method to update the JSON file"""
+        line = line.split()
+        if len(line) == 0:
+            print('** class name missing **')
+            return
+        elif line[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        elif len(line) == 1:
+            print('** instance id missing **')
+            return
+        else:
+            key = line[0] + '.' + line[1]
+            if key in models.data.all():
+                if len(line) > 2:
+                    if len(line) == 3:
+                        print('** value missing **')
+                    else:
+                        setattr(models.data.all()[key], line[2], line[3][1:-1])
+                        models.data.all()[key].save()
+                else:
+                    print('** attribute name missing **')
+            else:
+                print('** no instance found **')
 
     def do_destroy(self, line):
-        pass
+        """ Method to delete instance with class and id """
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+        arg_list = line.split()
+        try:
+            obj = eval(arg_list[0])
+        except Exception:
+            print("** class doesn't exist **")
+            return
+        if len(arg_list) == 1:
+            print('** instance id missing **')
+            return
+        if len(arg_list) > 1:
+            key = arg_list[0] + '.' + arg_list[1]
+            if key in models.data.all():
+                models.data.all().pop(key)
+                models.data.save()
+            else:
+                print('** no instance found **')
+                return
 
-    def do_all(self, line):
-        pass
+    def do_count(self, line):
+        """  retrieves the number of instances of a class """
+        class_token = line.split()
+        data = models.data.all()
+        instances = 0
+        if class_token[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        else:
+            for key in data:
+                className = key.split('.')
+                if className[0] == class_token[0]:
+                    instances = instances + 1
+            print(instances)
 
     def do_quit(self, line):
         """Quit the console"""
@@ -76,6 +140,10 @@ class HBNBCommand(cmd.Cmd):
         """Quit the console"""
         time.sleep(1)
         exit()
+
+    def emptyline(self):
+        """ What happens when empty line is passed """
+        pass
 
 
 if __name__ == "__main__":
